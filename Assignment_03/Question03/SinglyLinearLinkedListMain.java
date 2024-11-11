c99d008eddc3e399c5f5f5257f7152d74a7026e3
manishk1199 <manishkarne1967@gmail.com>
2024-11-12 00:05:08 +0530

    Assignment_03  Question_03 Uploaded

---
+++ Assignment_03/Question03/SinglyLinearLinkedListMain.java
@@ -0,0 +1,18 @@
+package com.manish;
+
+public class SinglyLinearLinkedListMain {
+
+	public static void main(String[] args) {
+		SinglyLinearLinkedList list = new SinglyLinearLinkedList();
+
+		list.addBeforeData(11, 22);
+		list.addBeforeData(22, 11);
+		list.addBeforeData(33, 77);
+		list.addAfterData(44, 33);
+		list.addAfterData(55, 22);
+
+		list.display();
+
+	}
+
+}
